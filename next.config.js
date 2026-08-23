bf82a210dde9d621f3221bf522d99d337fecb434
temaspirin09-lang <tema.spirin.09@mail.ru>
2026-08-23 22:29:58 +0300

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: { serverActions: { bodySizeLimit: '10mb' } },
+  images: {
+    remotePatterns: [{ protocol: 'https', hostname: '**' }]
+  }
+};
+
+module.exports = nextConfig;
